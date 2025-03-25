481e085ca8d75f7f09a98b363759d8d518accdbb
ananthu <ananthusmk@gmail.com>
2025-03-25 09:52:55 +0530

    done

--- src/components/DashBoard/styledComponents.js
+++ src/components/DashBoard/styledComponents.js
@@ -2,20 +2,19 @@ import styled from "styled-components";
 
 export const WrapperContainer = styled.div`
   width: 100%;
-`
+`;
 
 export const DashBoardContainer = styled.div`
   height: 90vh;
   width: 100%;
-  max-width: 100%;
   overflow-y: auto;
-  overflow-x: hidden;
   background-color: #282323;
-  padding: 10px;
+  padding: 20px;
+  box-sizing: border-box;
 
   @media (max-width: 768px) {
     height: 94vh;
-    padding: 5px;
+    padding: 10px;
   }
 `;
 
@@ -28,7 +27,7 @@ export const LoaderContainer = styled.div`
   @media (max-width: 768px) {
     height: 94vh;
   }
-`
+`;
 
 export const SubHeading = styled.h2`
   font-size: 28px;
@@ -36,53 +35,53 @@ export const SubHeading = styled.h2`
   color: #fff;
   font-family: 'Roboto';
   line-height: 24px;
-  padding-top: 40px;
-  padding-left: 70px;
+  padding-top: 20px;
+  padding-left: 20px;
 
   @media (max-width: 768px) {
     font-size: 20px;
     padding-left: 10px;
-    padding-top: 20px;
+    padding-top: 10px;
   }
 `;
 
 export const ListContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 20px;
-  margin-top: 30px;
-  margin-left: 40px;
-  width: calc(100% - 80px);
-  overflow-x: hidden;
+  margin-top: 20px;
+  padding-left: 20px;
+  width: calc(100% - 40px);
 
   @media (max-width: 768px) {
-    margin-left: 5px;
-    margin-right: 5px;
+    padding-left: 5px;
     width: calc(100% - 10px);
-    flex-wrap: wrap;
   }
 `;
 
 export const LocationContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 10px;
+  gap: 20px;
   padding: 10px;
-  margin-top: 30px;
-  margin-left: 60px;
-  width: 70%;
+  margin-top: 20px;
+  padding-left: 20px;
+  width: calc(100% - 40px);
+  box-sizing: border-box;
 
   @media (max-width: 1024px) {
     grid-template-columns: repeat(2, 1fr);
-    width: 90%;
-    margin-left: 40px;
-    gap: 5px;
+    width: calc(100% - 30px);
+    padding-left: 15px;
+    gap: 15px;
   }
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, 1fr);
     width: 100%;
-    margin-left: 0;
-    gap: 5px;
+    padding-left: 0;
+    gap: 10px;
+    padding: 5px;
   }
 `;
 
@@ -92,16 +91,15 @@ export const Cards = styled.div`
   border: 1px solid gray; 
   background-color: transparent;
   outline: none;
-  padding-top: 30px;
-  padding-left: 40px;
+  padding-top: 20px;
+  padding-left: 20px;
   flex-shrink: 1;
 
   @media (max-width: 768px) {
     width: 80px;
-     
     height: 100px;
-    padding-top: 20px;
-    padding-left: 20px;
+    padding-top: 10px;
+    padding-left: 10px;
   }
 `;
 
@@ -129,7 +127,6 @@ export const LocationImage = styled.img`
   width: 100%; 
   height: 160px;
   padding-bottom: 7px;
-  
 `;
 
 export const CardsTitle = styled.h3`
